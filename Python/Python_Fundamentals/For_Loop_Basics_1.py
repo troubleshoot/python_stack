89c8dd65649abe658a9ce3811a3738e551fa5c97
Drew Maichel <59732004+troubleshoot@users.noreply.github.com>
2021-03-08 15:45:35 -0800

    Add files via upload

---
+++ Python/Python_Fundamentals/For_Loop_Basics_1.py
@@ -0,0 +1,40 @@
+# i = 0
+# while i < 151:
+#   print(i)
+#   i = i + 1
+
+# i = 5
+# while i <= 1000:
+#   print(i)
+#   i = i + 5
+
+# i = 1
+# while i <= 100:
+#   if i % 10 == 0:
+#     print("Coding Dojo")
+#   elif i % 5 == 0:
+#     print("Coding")
+#   else:
+#     print(i)
+#   i = i + 1
+
+# count = 0
+# i = 0
+# while i <= 500000:
+#   if i % 2 != 0:
+#     count = count + i
+#   i = i + 1
+# print(count)
+
+# i = 2018
+# while i > 0:
+#   print (i)
+#   i = i - 4
+
+# def flexCounter(lowNum, highNum, mult):
+#   while lowNum <= highNum:
+#     if lowNum % mult == 0:
+#       print(lowNum)
+#     lowNum = lowNum + 1
+
+# flexCounter(2,9,3)
