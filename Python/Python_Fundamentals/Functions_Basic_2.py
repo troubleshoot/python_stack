10bdd1f2b57ad3f2367828be246245afdad846cd
Drew Maichel <59732004+troubleshoot@users.noreply.github.com>
2021-03-09 17:15:27 -0800

    Add files via upload

---
+++ Python/Python_Fundamentals/Functions_Basic_2.py
@@ -0,0 +1,36 @@
+# def countdown(x):
+#   while x >= 0:
+#     print(x)
+#     x = x - 1
+# countdown(5)
+
+# def printAndReturn(list):
+#   print(list[0])
+#   return(list[1])
+# print(printAndReturn([1,2]))
+
+# def firstPlusLength(list):
+#   return((list[0])+(len(list)))
+# print(firstPlusLength([1,2,3,4,5]))
+
+# def valuesGreaterThanSecond(list):
+#   i = 0
+#   count = 0
+#   arr = []
+#   while i < len(list):
+#     if(list[i] > list[1]):
+#       count = count + 1
+#       arr.append(list[i])
+#     i = i + 1
+#   print(count)
+#   return arr
+# print(valuesGreaterThanSecond([5,2,3,2,1,4]))
+
+# def thisLengthThatValue(l, v):
+#   i = 0
+#   arr = []
+#   while i < l:
+#     arr.append(v)
+#     i = i + 1
+#   return arr
+# print(thisLengthThatValue(4,7))
